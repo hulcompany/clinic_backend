908b6999f6ccdd5a61ae7a86601f95515ff9308d
hulcompany <hul.solution@gmail.com>
2026-01-31 03:32:36 -0800

    Create PaymentMethod.js

---
+++ src/models/PaymentMethod.js
@@ -0,0 +1,61 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+
+const PaymentMethod = sequelize.define('PaymentMethod', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    field: 'id'
+  },
+  name: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    field: 'name'
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+    field: 'description'
+  },
+  account_number: {
+    type: DataTypes.STRING(50),
+    allowNull: true,
+    field: 'account_number'
+  },
+  account_name: {
+    type: DataTypes.STRING(100),
+    allowNull: true,
+    field: 'account_name'
+  },
+  bank_name: {
+    type: DataTypes.STRING(100),
+    allowNull: true,
+    field: 'bank_name'
+  },
+  qr_code: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    field: 'qr_code' // Image filename for QR code
+  },
+  is_active: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+    field: 'is_active'
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+    field: 'created_at'
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+    field: 'updated_at'
+  }
+}, {
+  tableName: 'payment_methods',
+  timestamps: false
+});
+
+module.exports = PaymentMethod;
