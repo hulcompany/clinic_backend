73ee843c7cfe0e9640d9df4db982f0faf61c0365
hulcompany <hul.solution@gmail.com>
2026-03-10 15:23:52 -0700

    Update index.js

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -6,34 +6,35 @@
  */
 
 // Import v1 routes directly (since they're not version-specific yet)
-const authRoutes = require('../../routes/authentication/auth.routes');
-const userRoutes = require('../../routes/authentication/user.routes');
-const adminRoutes = require('../../routes/authentication/admin.routes');
-const consultationRoutes = require('../../routes/consultation.routes');
-const chatRoutes = require('../../routes/chat/chat.routes');
-const contactUsRoutes = require('../../routes/contactUs.routes');
-const messageRoutes = require('../../routes/chat/message.routes');
-const realtimeChatRoutes = require('../../routes/chat/realtime.chat.routes');
-const reviewRoutes = require('../../routes/review.routes');
-const serviceRoutes = require('../../routes/service.routes');
-const sessionRoutes = require('../../routes/session.routes');
-const availabilityRoutes = require('../../routes/availability.routes');
-const medicalRecordRoutes = require('../../routes/medicalRecord.routes');
-const blogRoutes = require('../../routes/blog.routes');
-const notificationRoutes = require('../../routes/notification.routes');
-const landingImageRoutes = require('../../routes/landingImage.routes');
-const phoneVerificationRoutes = require('../../routes/authentication/phoneVerification.routes');
-const dashboardRoutes = require('../../routes/dashboard.routes');
-const paymentRoutes = require('../../routes/payment.routes');
-const paymentMethodRoutes = require('../../routes/paymentMethod.routes');
+const authRoutes= require('../../routes/authentication/auth.routes');
+const userRoutes= require('../../routes/authentication/user.routes');
+const adminRoutes= require('../../routes/authentication/admin.routes');
+const consultationRoutes= require('../../routes/consultation.routes');
+const chatRoutes= require('../../routes/chat/chat.routes');
+const contactUsRoutes= require('../../routes/contactUs.routes');
+const messageRoutes= require('../../routes/chat/message.routes');
+const realtimeChatRoutes= require('../../routes/chat/realtime.chat.routes');
+const reviewRoutes= require('../../routes/review.routes');
+const serviceRoutes= require('../../routes/service.routes');
+const sessionRoutes= require('../../routes/session.routes');
+const availabilityRoutes= require('../../routes/availability.routes');
+const medicalRecordRoutes= require('../../routes/medicalRecord.routes');
+const blogRoutes= require('../../routes/blog.routes');
+const notificationRoutes= require('../../routes/notification.routes');
+const landingImageRoutes= require('../../routes/landingImage.routes');
+const phoneVerificationRoutes= require('../../routes/authentication/phoneVerification.routes');
+const dashboardRoutes= require('../../routes/dashboard.routes');
+const paymentRoutes= require('../../routes/payment.routes');
+const paymentMethodRoutes= require('../../routes/paymentMethod.routes');
+const beforeAfterRoutes= require('../../routes/beforeAfter.routes');
 
 module.exports = {
   authRoutes,
   userRoutes,
   adminRoutes,
-  consultationRoutes,
+ consultationRoutes,
   chatRoutes,
-  contactUsRoutes,
+ contactUsRoutes,
   messageRoutes,
   realtimeChatRoutes,
   reviewRoutes,
@@ -47,5 +48,6 @@ module.exports = {
   phoneVerificationRoutes,
   dashboardRoutes,
   paymentRoutes,
-  paymentMethodRoutes
+  paymentMethodRoutes,
+  beforeAfterRoutes
 };
